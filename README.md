9ffc9580a65160a7428c6e2c8703db0ceb9ffc54
jahjahway <suge15@sina.cn>
2021-11-15 19:57:04 +0800

    Init project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# CafeOS
+## Cafe OS
+Kernal should not be a black box for java developer.
+
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Kernal should not be a black box for java developer\n");
+    return 0;
+}
